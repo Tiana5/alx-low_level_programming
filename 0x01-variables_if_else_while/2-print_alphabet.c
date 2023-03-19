ca3f855f911d6186ad573b4f70c23706847b8f97
Tiana5 <awino.tiana@gmail.com>
2023-03-18 17:32:38 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{       int t = 97;
+
+	while (t >= 122)
+
+			putchar(t);
+		t++;
+	putchar("\n")
+	return (0);
+}
+
+
