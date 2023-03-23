b55e4985e5ff1d27daf743d6755713c1f9b0b47d
Tiana5 <awino.tiana@gmail.com>
2023-03-23 10:26:53 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* positive_or_negative.c - print positive or negative interger
+* @i: number to be checked
+* Return: alays 0
+*/
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d id positive\n", i);
+}
+
