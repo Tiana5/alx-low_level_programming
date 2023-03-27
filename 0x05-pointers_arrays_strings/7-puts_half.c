5e6cf805a4e4db9f6b2386ff7e0360fd75c42584
Tiana5 <awino.tiana@gmail.com>
2023-03-27 12:50:35 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*puts_half - print half of a string
+*@str: parameter
+*Return: 0
+*/
+void puts_half(char *str)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		count++;
+	if (count % 2 == 0)
+	{
+		for (i = count / 2; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+	else if (count % 2 != 0)
+	{
+		for (i = (count - 1) / 2 ; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
