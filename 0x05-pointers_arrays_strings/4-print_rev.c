7f65614940e97307511cea25cb1f169bef1e7912
Tiana5 <awino.tiana@gmail.com>
2023-03-27 08:38:45 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_rev - print a string in reverse with a new line
+*@s: string to be printed
+*Return: 0
+*/
+void print_rev(char *s)
+{
+	int t;
+	int c;
+
+	for (t = 0 ; s[t] != '\0' ; t++)
+		c++;
+	for (t = c ; t >= 0 ; t--)
+		_putchar(s[t]);
+	_putchar('\n')
+}
