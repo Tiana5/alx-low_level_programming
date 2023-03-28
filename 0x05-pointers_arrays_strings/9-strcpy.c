7012e03442ffc4c09911a59d08fa528106389c51
Tiana5 <awino.tiana@gmail.com>
2023-03-28 12:30:45 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,8 +9,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	if (dest == NULL);
-		return(NULL);
+	if (dest == NULL)
+		return (NULL);
 
 	for (i = 0 ; src[i] != '\0' ; i++)
 		dest[i] = src[i];
